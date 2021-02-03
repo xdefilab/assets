7b707798eb66067bd6b02900ebb095db2be4efb1
jingleizhang <eric@xdefi.com>
2021-02-03 22:40:33 +0800

    add kovan tokens

--- script/build_ethereum_and_kovan.sh
+++ script/build_ethereum_and_kovan.sh
@@ -1,6 +1,10 @@
 
 rm -rf ../build
 
+mkdir -p ../build/blockchains/ethereum
+
+cp -rfv ../blockchains/ethereum/* ../build/blockchains/ethereum
+
 mkdir -p ../build/blockchains/kovan
 
 cp -rfv ../blockchains/kovan/* ../build/blockchains/kovan
